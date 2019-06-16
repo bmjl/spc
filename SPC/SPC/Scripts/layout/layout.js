1d8078929ebdda7698891381a727e6fba04648ef
陈磊 <304102966@QQ.COM>
2019-06-16 12:46:56 +0800

    3

---
+++ SPC/SPC/Scripts/layout/layout.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $(".title_ul").children().click(function () {
+        $this = $(this);
+        $this.parent().children("li").removeClass("active");
+        $this .addClass("active");
+    });
+
+
+})
---
+++ SPC/SPC/Scripts/x-r/x-r.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $(".btn").click(function () {
+        $(this).button('loading').delay(1000).queue(function () {
+            $(this).button('reset');
+            $(this).dequeue();
+        });
+    });
+    $(".div_collapse").click(function () {
+        var s = $(this).attr("state");
+        if (s == 1) {
+            $(this).attr("state", 0);
+            $(this).children().children().text("收起");
+        } else {
+            $(this).attr("state", 1);
+            $(this).children().children().text("展开,输入或修改参数");
+        }
+    })
+
+});
