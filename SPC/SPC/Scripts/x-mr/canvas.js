63031dd94efa2b007a5d19f24161d4f5f4ca30a3
chenlei09 <chenlei09@cnpc.com.cn>
2019-06-20 16:28:04 +0800

    x-mr 20190620 16:28

---
+++ SPC/SPC/Scripts/x-mr/canvas.js
@@ -0,0 +1,148 @@
+﻿$(function() {
+    X();
+    S();
+});
+
+function X() {
+    //1,获取值
+    var arr = $(".X");
+    var arr1 = [];
+    arr.each(function() {
+        arr1.push($(this).text());
+    });
+    var canvasX = $("#canvasX")[0];
+    var contextX = canvasX.getContext('2d');
+    var Max = 20.00, //最大值
+        interval = 5, //间隔
+        Min = 0;
+    var ii = 5,
+        jj = arr1.length + 2;
+    var hight = 60; //每行的高度
+    var width = 40; //每列的宽度
+    Draw(contextX, Max, interval, arr1, ii, jj, Min, width, hight, 'x');
+}
+
+function S() {
+    //1,获取值
+    var arr = $(".S");
+    var arr1 = [];
+    arr.each(function() {
+        arr1.push($(this).text());
+    });
+    var canvasR = $("#canvasR")[0];
+    var contextR = canvasR.getContext('2d');
+
+    var Max = 15.00, //最大值
+        interval = 5.00, //间隔
+        Min = 0;
+    var ii = 4,
+        jj = arr1.length + 2;
+    var hight = 80; //每行的高度
+    var width = 40; //每列的宽度
+    Draw(contextR, Max, interval, arr1, ii, jj, Min, width, hight, 's');
+}
+
+function Draw(cxt, Max, interval, arr1, ii, jj, min, width, hight, type) {
+    cxt.font = "14px 宋体"; //css font属性
+    cxt.textBaseline = "middle";
+    //画线
+    var y1, y2;
+    var x1 = 50,
+        x2 = jj * width + 10,
+        yy = 30;
+
+    for (var i = 0; i < ii; i++) {
+        var y = i * hight + yy;
+
+        if (i == 0 || i == ii - 1) {
+            if (i == 0) {
+                y1 = y;
+            } else {
+                y2 = y;
+            }
+            cxt.strokeStyle = "black"; //设置画笔的颜色
+        } else if (i == 1 || i == 5) {
+            cxt.strokeStyle = "blue";
+        } else if (i == 2 || i == 4) {
+            cxt.strokeStyle = "red";
+        } else if (i == 3) {
+            cxt.strokeStyle = "yellow";
+        }
+
+        cxt.beginPath(); //开启新路径				
+        cxt.lineWidth = 1; //设定画笔的宽度				
+
+        cxt.moveTo(x1, y); //设定笔触的位置				
+        cxt.lineTo(x2, y); //设置移动的方式				
+        cxt.stroke(); //画线				
+        cxt.closePath(); //封闭路径
+
+        //写数字
+        var fix = fixed(Max - i * interval);
+        cxt.fillText(fix, x1 - 35, y);
+    }
+
+
+
+    cxt.beginPath(); //开启新路径				
+    cxt.lineWidth = 1; //设定画笔的宽度
+    cxt.moveTo(x1, y1); //设定笔触的位置				
+    cxt.lineTo(x1, y2); //设置移动的方式
+    cxt.moveTo(x2, y1); //设定笔触的位置				
+    cxt.lineTo(x2, y2); //设置移动的方式				
+    cxt.stroke(); //画线				
+    cxt.closePath(); //封闭路径
+
+    cxt.textAlign = "center";
+    for (var j = 0; j < jj; j++) {
+        var x = j * width + x1;
+        cxt.beginPath(); //开启新路径				
+        cxt.lineWidth = 1; //设定画笔的宽度				
+        cxt.strokeStyle = "black"; //设置画笔的颜色
+        cxt.moveTo(x, y2 - 3); //设定笔触的位置				
+        cxt.lineTo(x, y2); //设置移动的方式				
+        cxt.stroke(); //画线				
+        cxt.closePath(); //封闭路径
+
+        //写数字
+        cxt.fillText(j + 1, x, y2 + 14);
+    }
+    var p_y;
+    for (var k = 0; k < arr1.length; k++) {
+        //p_y = (arr1[k] - 0.40) * 10 * 20 + 30;
+        // var p_y = (ii - 1) * hight + width + 30 - ((arr1[k] - min) * 10 * hight + 30);
+        var p_y = (ii - 1) * hight + yy - ((arr1[k] - min) / interval * hight);
+
+        //划线
+        if (k == 0 || (k == arr1.length - 1 && type == 'x')) {} else if (k == 1 && type == 's') {} else {
+            //画点
+            arc(cxt, k * width + x1, p_y);
+            //画线
+            cxt.beginPath(); //开启新路径				
+            cxt.lineWidth = 1; //设定画笔的宽度				
+            cxt.strokeStyle = "blue"; //设置画笔的颜色
+            cxt.moveTo((k - 1) * width + x1, (ii - 1) * hight + yy - ((arr1[k - 1] - min) / interval * hight)); //设定笔触的位置
+            //cxt.moveTo((k - 1) * width + x1, (ii - 1) * hight + width + 30 - ((arr1[k - 1] - min) * 10 * hight + 30)); //设定笔触的位置
+            cxt.lineTo(k * width + x1, p_y); //设置移动的方式	
+            cxt.stroke(); //画线				
+            cxt.closePath(); //封闭路径
+        }
+    }
+
+
+}
+
+//保留两位小数
+function fixed(num) {
+    return num.toFixed(2);
+}
+
+//在指定的位置上花一个点
+function arc(cxt, x, y) {
+    cxt.beginPath();
+    cxt.fillStyle = "rgb(0,0,255)"; //设置填充的颜色
+    cxt.fillRect(x - 2, y - 2, 5, 5);
+    cxt.fill();
+    cxt.stroke();
+    cxt.closePath();
+}
---
+++ SPC/SPC/Scripts/x-mr/x-mr.js
@@ -0,0 +1,95 @@
+﻿$(function() {
+    $(".btn").click(function() {
+        $(this).button('loading').delay(1000).queue(function() {
+            $(this).button('reset');
+            $(this).dequeue();
+        });
+    });
+    $(".div_collapse").click(function() {
+        var s = $(this).attr("state");
+        if (s == 1) {
+            $(this).attr("state", 0);
+            $(this).children().children().text("收起");
+        } else {
+            $(this).attr("state", 1);
+            $(this).children().children().text("展开,输入或修改参数");
+        }
+    })
+
+
+
+    var arr = [
+        [8.00, 8.50, 7.40, 10.50, 9.30, 11.10, 10.40, 10.40, 9.00, 10.00, 11.70, 10.30, 16.20, 11.60, 11.50, 11.00, 12.00, 11.00, 10.20, 10.00, 10.50, 10.30, 11.50, 11.10, 0]
+    ];
+    var arr_xs = new Array();
+    //初始化，定下有多少行
+    for (i = 0; i < arr[0].length; i++) {
+        arr_xs[i] = [];
+    }
+
+    for (var m = 0; m < arr.length; m++) {
+        for (var n = 0; n < arr[m].length; n++) {
+            arr_xs[n][m] = arr[m][n];
+        }
+    }
+    var html = "";
+    var ii = 0;
+    var html_th = "";
+
+    if (arr_xs.length != 5) {
+        var x = 0;
+        $('#_thead').append('<th  class="border">序号</th>');
+        for (var i = 0; i < arr_xs[0].length; i++) {
+            x = i + 1;
+            $('#_thead').append('<th class="border" >' + x + '</th>');
+        }
+        $('#_thead').append('<th class="border">ΣＸ</th><th  class="border">Ｘ</th><th  class="border">S</th>');
+        $('#_head').hide();
+    }
+    var xs = new Array();
+    for (var i = 0; i < arr_xs.length; i++) {
+        ii = i + 1;
+        html += '<tr class="max_width">';
+        html += '<td> ' + ii + '</td >';
+        for (var j = 0; j < arr_xs[i].length; j++) {
+            html += '<td><input type="text" value="' + fixed(arr_xs[i][j]) + '"></td>';
+            xs[j] = arr_xs[i][j];
+        }
+        html += _js(i, xs);
+        html += '</tr>';
+        $("#parameter").append(html);
+        html = "";
+        $("#par").hide();
+    }
+});
+var first = 0;
+
+function _js(num, arr) {
+
+    var sum = 0;
+    //自身相乘后再累加
+    var sum_all = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        sum_all += arr[i] * arr[i];
+    }
+    // var R = Math.max.apply(null, arr) - Math.min.apply(null, arr);
+    var X = sum / arr.length;
+
+
+    var S = 0;
+    if (num == 0) {
+
+    } else {
+        // S = Math.sqrt((sum_all - arr.length * X * X) / (arr.length - 1));
+        S = Math.abs(sum - first);
+    }
+
+    var html = '<td class="sum">' + sum.toFixed(2) + '</td><td class="X">' + X.toFixed(2) + '</td><td class="S">' + S.toFixed(2) + '</td>';
+    first = arr[0];
+    return html;
+}
+
+function fixed(num) {
+    return num.toFixed(2);
+}
